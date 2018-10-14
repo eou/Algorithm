3a00fec0017bde42e3ea0508d9060b127cf5f0f5
eou <whythuss@gmail.com>
2018-10-14 14:20:59 -0500

    😰

--- Java/273. Integer to English Words.java
+++ Java/273. Integer to English Words.java
@@ -43,33 +43,34 @@ class Solution {
 }
 
 class Solution {
-    // 将递归写得更加彻底一些
-    private final String[] belowTen = new String[] { "", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine" };
-    private final String[] belowTwenty = new String[] { "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen" };
-    private final String[] belowHundred = new String[] { "", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety" };
+    // 更彻底的递归解法
+    private final String[] LESS_THAN_20 = { "", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen" };
+    private final String[] TENS = new String[] { "", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety" };
 
     public String numberToWords(int num) {
-        if (num == 0)
+        if (num == 0) {
             return "Zero";
-        return helper(num);
+        } else {
+            return helper(num);
+        }
     }
 
     private String helper(int num) {
-        String result = new String();
-        if (num < 10)
-            result = belowTen[num];
-        else if (num < 20)
-            result = belowTwenty[num - 10];
-        else if (num < 100)
-            result = belowHundred[num / 10] + " " + helper(num % 10);
-        else if (num < 1000)
+        String result;
+        if (num < 20) {
+            result = LESS_THAN_20[num];
+        } else if (num < 100) {
+            result = TENS[num / 10] + " " + helper(num % 10);
+        } else if (num < 1000) {
             result = helper(num / 100) + " Hundred " + helper(num % 100);
-        else if (num < 1000000)
+        } else if (num < 1000000) {
             result = helper(num / 1000) + " Thousand " + helper(num % 1000);
-        else if (num < 1000000000)
+        } else if (num < 1000000000) {
             result = helper(num / 1000000) + " Million " + helper(num % 1000000);
-        else
+        } else {
             result = helper(num / 1000000000) + " Billion " + helper(num % 1000000000);
+        }
+        // 注意去掉空格
         return result.trim();
     }
 }
---
+++ Java/4. Median of Two Sorted Arrays.java
@@ -0,0 +1,98 @@
+// 4. Median of Two Sorted Arrays
+// 不同的代码基本都是二分思路，时间复杂度log(m+n)，不同之处在于找第k个元素的方式
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int len = nums1.length + nums2.length;
+        if (len % 2 == 0) {
+            return (findKth(nums1, 0, nums2, 0, len / 2) + findKth(nums1, 0, nums2, 0, len / 2 + 1)) / 2;
+        }
+        return findKth(nums1, 0, nums2, 0, len / 2 + 1);
+    }
+
+    double findKth(int[] nums1, int start1, int[] nums2, int start2, int k) {
+        int len1 = nums1.length - start1;
+        int len2 = nums2.length - start2;
+        // assume the length of nums1 should always no longer than the length of nums2
+        if (len1 > len2) {
+            return findKth(nums2, start2, nums1, start1, k);
+        }
+        if (len1 == 0) {
+            return nums2[start2 + k - 1];
+        }
+        if (len2 == 0) {
+            return nums1[start1 + k - 1];
+        }
+
+        // Base Case. 在这里必须要退出。因为k = 1的时候，不可能再分了。
+        if (k == 1) {
+            return Math.min(nums1[start1], nums2[start2]);
+        }
+        
+        // [1,7], [0,2,3,4,5,6,7,9,11], 11/2 = 5
+        // pa = 2, pb = 3 => value: nums1: 7, nums2: 3 => start1: 0, start2: 3 => newk: 2
+        // pa = 1, pb = 1 => value: nums1: 1, nums2: 4 => start1: 1, start2: 3 => newk: 1
+        // len1 == 0 => nums2[start2 + k - 1] = 4
+        int pa = Math.min(k / 2, len1);
+        int pb = k - pa;
+        if (nums1[start1 + pa - 1] < nums2[start2 + pb - 1]) {
+            // we should discard the numbers before nums1[start1 + pa - 1], so the new start1 should be start1 + pa
+            return findKth(nums1, start1 + pa, nums2, start2, k - pa);
+        } else if (nums1[start1 + pa - 1] > nums2[start2 + pb - 1]) {
+            return findKth(nums1, start1, nums2, start2 + pb, k - pb);
+        } else {
+            return nums1[start1 + pa - 1];
+        }
+    }
+}
+
+class Solution {
+    // 不同版本的 findKth() 对于二分的实现略有不同，其他一致
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int len = nums1.length + nums2.length;
+        if (len % 2 == 0) {
+            return (findKth(nums1, 0, nums2, 0, len / 2) + findKth(nums1, 0, nums2, 0, len / 2 + 1)) / 2;
+        }
+        return findKth(nums1, 0, nums2, 0, len / 2 + 1);
+    }
+
+    // Find the Kth large number.
+    double findKth(int[] nums1, int start1, int[] nums2, int start2, int k) {
+        int len1 = nums1.length - start1;
+        int len2 = nums2.length - start2;
+
+        if (len1 == 0) {
+            return nums2[start2 + k - 1];
+        }
+        if (len2 == 0) {
+            return nums1[start1 + k - 1];
+        }
+
+        if (k == 1) {
+            return Math.min(nums1[start1], nums2[start2]);
+        }
+
+        // -1是因为索引本身是从0开始的。而前k大元素含有k个元素。
+        int mid = k / 2 - 1;
+        // [1,7], [0,2,3,4,5,6,7,9,11,12,13], 13/2 = 6
+        // mid = 2 => keyA = MAX, keyB = 3 => start1: 0, start2: 3, kNew: 3
+        // mid = 0 => keyA = 1, keyB = 4 => start1: 1, start2: 3, kNew: 2
+        // len1 == 0 => nums2[start2 + k - 1] = 4
+        
+        // [6], [0,2,3,4,5,7,9,11,12,13], 11/2 = 5
+        // mid = 1 => keyA = MAX, keyB = 2 => start1: 0, start2: 2, kNew: 3
+        // mid = 0 => keyA = 6, keyB = 3 => start1: 0, start2: 3, kNew: 2
+        // mid = 0 => keyA = 6, keyB = 4 => start1: 0, start2: 4, kNew: 1
+        // k == 1 => nums1[0]
+        // 越界的时候key设为无穷大是为了丢弃对方数组的左边部分
+        int keyA = start1 + mid >= nums1.length ? Integer.MAX_VALUE : nums1[start1 + mid];
+        int keyB = start2 + mid >= nums2.length ? Integer.MAX_VALUE : nums2[start2 + mid];
+
+        // 因为要丢弃k / 2个元素，注意kNew != k / 2
+        int kNew = k - k / 2;
+        if (keyA < keyB) {
+            return findKth(nums1, start1 + k / 2, nums2, start2, kNew);
+        } else {
+            return findKth(nums1, start1, nums2, start2 + k / 2, kNew);
+        }
+    }
+}
