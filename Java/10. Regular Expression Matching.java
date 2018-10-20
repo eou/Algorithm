d99d497383f47afa1eb6deb8e719f01e04fbe7af
eou <whythuss@gmail.com>
2018-10-20 14:28:51 -0500

    🤒

---
+++ Java/10. Regular Expression Matching.java
@@ -0,0 +1,76 @@
+// 10. Regular Expression Matching
+// 类似91.Decode Ways
+class Solution {
+    // 递归版本，时间复杂度计算比较复杂为O((S+P)*2^(S+P/2))，可以看做O(n2^n)
+    public boolean isMatch(String s, String p) {
+        if(p.isEmpty()) {
+            return s.isEmpty();
+        }
+        
+        boolean singleMatch = (!s.isEmpty() && (p.charAt(0) == s.charAt(0) || p.charAt(0) == '.'));
+        
+        // 如果模式串长度至少为2
+        // 如果模式串第二个字符为*，有两个选择，因为*可以代表任意数量个前个字符，包括0个
+        // 可以当做模式串这两位不存在，继续匹配；也可以当第一位匹配成功，继续用来匹配匹配串的下一位
+        if(p.length() > 1 && p.charAt(1) == '*') {
+            return (isMatch(s, p.substring(2)) || (singleMatch && isMatch(s.substring(1), p)));
+        // 如果模式串第二个字符不为*，或者模式串长度为1，则正常匹配一位
+        } else {
+            return singleMatch && isMatch(s.substring(1), p.substring(1));
+        }
+    }
+}
+
+class Solution {
+    // DP版本，时间复杂度O(S*P)
+    // 二维DP数组有3个状态，1是匹配，0是未检测，-1是不匹配
+    int[][] dp;
+    public boolean isMatch(String s, String p) {
+        dp = new int[s.length() + 1][p.length() + 1];
+        return helper(0, 0, s, p);
+    }
+
+    private boolean helper(int i, int j, String s, String p) {
+        if (dp[i][j] != 0) {
+            return dp[i][j] == 1;
+        }
+
+        // 思路跟递归版本基本一致，仅仅是最后把结果保存在DP数组中作为中间结果，减少重复验证次数
+        boolean result;
+        if (j == p.length()) {
+            result = i == s.length();
+        } else {
+            boolean firstMatch = (i < s.length() && (p.charAt(j) == s.charAt(i) || p.charAt(j) == '.'));
+
+            if (j + 1 < p.length() && p.charAt(j + 1) == '*') {
+                result = (helper(i, j + 2, s, p) || (firstMatch && helper(i + 1, j, s, p)));
+            } else {
+                result = firstMatch && helper(i + 1, j + 1, s, p);
+            }
+        }
+
+        dp[i][j] = result == true ? 1 : -1;
+        return result;
+    }
+}
+
+class Solution {
+    public boolean isMatch(String s, String p) {
+        boolean[][] dp = new boolean[s.length() + 1][p.length() + 1];
+        dp[s.length()][p.length()] = true;
+
+        for (int i = s.length(); i >= 0; i--) {
+            for (int j = p.length() - 1; j >= 0; j--) {
+                boolean firstMatch = (i < s.length() && (p.charAt(j) == s.charAt(i) || p.charAt(j) == '.'));
+
+                if (j + 1 < p.length() && p.charAt(j + 1) == '*') {
+                    dp[i][j] = (dp[i][j + 2] || firstMatch && dp[i + 1][j]);
+                } else {
+                    dp[i][j] = (firstMatch && dp[i + 1][j + 1]);
+                }
+            }
+        }
+
+        return dp[0][0];
+    }
+}
---
+++ Java/923. 3Sum With Multiplicity.java
@@ -0,0 +1,56 @@
+// 923. 3Sum With Multiplicity
+class Solution {
+    // 思路与3Sum一样，但是需要提前去重，否则TLE
+    public int threeSumMulti(int[] A, int target) {
+        int M = 1_000_000_007;
+        
+        // 去重
+        long[] count = new long[101];
+        int uniq = 0;
+        for (int x: A) {
+            count[x]++;
+            if (count[x] == 1) {
+                uniq++;
+            }
+        }
+        
+        int[] keys = new int[uniq];
+        int t = 0;
+        for (int i = 0; i <= 100; ++i) {
+            if (count[i] > 0){
+                keys[t] = i;
+                t++;
+            }
+        }
+        
+        long result = 0;
+        for (int i = 0; i < keys.length; ++i) {
+            int x = keys[i];
+            int j = i, k = keys.length - 1;
+            while (j <= k) {
+                int y = keys[j], z = keys[k];
+                if (y + z < target - x) {
+                    j++;
+                } else if (y + z > target - x) {
+                    k--;
+                } else {
+                    if (i < j && j < k) {
+                        result += count[x] * count[y] * count[z];
+                    } else if (i == j && j < k) {
+                        result += count[x] * (count[x] - 1) / 2 * count[z];
+                    } else if (i < j && j == k) {
+                        result += count[x] * count[y] * (count[y] - 1) / 2;
+                    } else {
+                        result += count[x] * (count[x] - 1) * (count[x] - 2) / 6;
+                    }
+
+                    result %= M;
+                    j++;
+                    k--;
+                }
+            }
+        }
+
+        return (int) (result);
+    }
+}
