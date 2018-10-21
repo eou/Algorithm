03738c2f7a59a01b1d5232ff47b6127f303cfb74
eou <whythuss@gmail.com>
2018-10-21 11:44:57 -0500

    🎃

---
+++ Java/157. Read N Characters Given Read4.java
@@ -0,0 +1,31 @@
+// 157. Read N Characters Given Read4
+// 此题意义模糊，无特别算法，不知考察点在哪
+// 注意：char *buf is destination not source, similar to that of scanf("%s", buf), OJ outputs this buf value.
+public class Solution extends Reader4 {
+    /**
+     * @param buf Destination buffer
+     * @param n   Maximum number of characters to read
+     * @return    The number of characters read
+     */
+    public int read(char[] buf, int n) {
+        int total = 0;
+        char[] tmp = new char[4];
+
+        // 当 total == n 的时候停止
+        while (total < n) {
+            int count = read4(tmp);
+
+            // 把读入的字符存储进buf中，注意不是所有tmp中的字符都能存储，还要考虑最大读取上限 total < n
+            for (int i = 0; i < count && total < n; i++) {
+                buf[total++] = tmp[i];
+            }
+
+            // 如果此次读入已经不足4个字符，说明已经读完，要么达到最大读取上限，要么文件无剩余字符
+            if(count < 4) {
+                break;
+            }
+        }
+
+        return total;
+    }
+}
---
+++ Java/158. Read N Characters Given Read4 II - Call multiple times.java
@@ -0,0 +1,61 @@
+// 158. Read N Characters Given Read4 II - Call multiple times
+// 与 157 不同之处在于重复调用 read() 的时候会从上次读完的地方继续读
+// 上一次读取的内容不一定已经全部用上，所以用方法外的变量存储已经读取的数据和上一次读取结束的位置
+public class Solution extends Reader4 {
+    /**
+     * @param buf Destination buffer
+     * @param n   Maximum number of characters to read
+     * @return    The number of characters read
+     */
+    private int count = 0;
+    private int pointer = 0;
+    private char[] tmp = new char[4];
+
+    public int read(char[] buf, int n) {
+        int total = 0;
+        
+        while(total < n) {
+            if(pointer == 0) {
+                count = read4(tmp);
+            }
+            
+            while(total < n && pointer < count) {
+                buf[total++] = tmp[pointer++];
+            }
+            
+            if(pointer == count) {
+                pointer = 0;
+            }
+            
+            if(count < 4) {
+                break;
+            }
+        }
+        
+        return total;
+    }
+}
+
+public class Solution extends Reader4 {
+    // 另一个简洁版本
+    private int count = 0;
+    private int pointer = 0;
+    private char[] tmp = new char[4];
+
+    public int read(char[] buf, int n) {
+        // Pointer to external buffer
+        int total = 0;
+        while (total < n) {
+            // if pointer < count, which means there are still remaining datas in tmp since last read
+            if (pointer == count) {
+                pointer = 0; // Reset interval buffer's pointer to the beginning
+                count = read4(tmp);
+                if (count == 0) {
+                    break;
+                }  
+            }
+            buf[total++] = tmp[pointer++];
+        }
+        return total;
+    }
+}
