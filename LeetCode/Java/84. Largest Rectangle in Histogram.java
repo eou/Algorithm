f4f682a877444fdc7eb8e0603615f8728c58ee10
eou <whythuss@gmail.com>
2019-01-10 15:41:24 -0600

    ○

---
+++ LeetCode/Java/84. Largest Rectangle in Histogram.java
@@ -0,0 +1,107 @@
+// 84. Largest Rectangle in Histogram
+class Solution {
+    // Brute force 有几种，从左往右，从右往左，从中间往两边，时间复杂度为 O(n^2)，可以 AC
+    public int largestRectangleArea(int[] heights) {
+        int maxArea = 0;
+        for(int i = 0; i < heights.length; i++) {
+            int minHeight = heights[i];
+            // 从左往右遍历
+            for(int j = i; j < heights.length; j++) {
+                minHeight = Math.min(minHeight, heights[j]);
+                maxArea = Math.max(maxArea, minHeight * (j - i + 1));
+            }
+        }
+        return maxArea;
+    }
+}
+
+class Solution {
+    // Brute force，从右往左
+    public int largestRectangleArea(int[] heights) {
+        int maxArea = 0;
+        for (int i = 0; i < heights.length; i++) {
+            // 剪枝
+            if (i < heights.length - 1 && heights[i] <= heights[i + 1]) {
+                continue;
+            }
+            int minHeight = heights[i];
+            // 从右往左遍历
+            for (int j = i; j >= 0; j--) {
+                minHeight = Math.min(minHeight, heights[j]);
+                maxArea = Math.max(maxArea, minHeight * (i - j + 1));
+            }
+        }
+        return maxArea;
+    }
+}
+
+class Solution {
+    // divide and conquer，时间复杂度为 O(nlogn)
+    public int largestRectangleArea(int[] heights) {
+        return helper(heights, 0, heights.length - 1);
+    }
+
+    private int helper(int[] heights, int start, int end) {
+        if(start > end) {
+            return 0;
+        }
+        
+        int minIndex = start;
+        for(int i = start; i <= end; i++) {
+            if(heights[minIndex] > heights[i]) {
+                minIndex = i;
+            }
+        }
+        
+        return Math.max(heights[minIndex] * (end - start + 1),
+                Math.max(helper(heights, start, minIndex - 1),
+                helper(heights, minIndex + 1, end)));
+    }
+}
+
+class Solution {
+    // 单调增栈版本，时间复杂度为 O(n)
+    // 一个很明显的能用 O(n) 解法求直方图面积的特例就是单调增直方图，于是将此作为出发点
+    public int largestRectangleArea(int[] heights) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        stack.push(-1); // 给 heights 尾部添加 0 也行，但这个操作 Java 没 C++ 方便
+        int maxArea = 0;
+        for (int i = 0; i < heights.length; i++) {
+            // 维护一个递增栈
+            if (stack.peek() == -1 || heights[i] > heights[stack.peek()]) {
+                stack.push(i);
+            } else {
+                maxArea = Math.max(maxArea, heights[stack.pop()] * (i - stack.peek() - 1));
+                i--; // 外层 for 循环会 i++; 保持指针位置不变
+            }
+        }
+        while (stack.peek() != -1) {
+            maxArea = Math.max(maxArea, heights[stack.pop()] * (heights.length - stack.peek() - 1));
+        }
+
+        return maxArea;
+    }
+}
+
+class Solution {
+    // 另一种简洁的写法
+    public int largestRectangleArea(int[] heights) {
+        if (heights == null || heights.length == 0) {
+            return 0;
+        }
+
+        Deque<Integer> stack = new ArrayDeque<>();
+        int maxArea = 0;
+        for (int i = 0; i <= heights.length; i++) {
+            int cur = (i == heights.length) ? -1 : heights[i];
+            while (!stack.isEmpty() && cur <= heights[stack.peek()]) {
+                int h = heights[stack.pop()];
+                int w = stack.isEmpty() ? i : i - stack.peek() - 1;
+                maxArea = Math.max(maxArea, h * w);
+            }
+            stack.push(i);
+        }
+
+        return maxArea;
+    }
+}
---
+++ LeetCode/Java/85. Maximal Rectangle.java
@@ -0,0 +1,134 @@
+// 85. Maximal Rectangle
+class Solution {
+    // 与 84. Largest Rectangle in Histogram 递增栈版本的思路完全一样，这里时间复杂度为 O(n^2)
+    public int maximalRectangle(char[][] matrix) {
+        if (matrix.length == 0 || matrix[0].length == 0) {
+            return 0;
+        }
+        int maxArea = 0;
+        for(int i = 0; i < matrix.length; i++) {
+            maxArea = Math.max(maxArea, helper(matrix, i));
+        }
+        return maxArea;
+    }
+    
+    // calculate largest rectangle in histogram based on each row
+    private int helper(char[][] matrix, int row) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        stack.push(-1);
+        int maxArea = 0;
+        for(int i = 0; i < matrix[0].length; i++) {
+            if(stack.peek() == -1 || height(matrix, row, i) > height(matrix, row, stack.peek())) {
+                stack.push(i);
+            } else {
+                maxArea = Math.max(maxArea, height(matrix, row, stack.pop()) * (i - stack.peek() - 1));
+                i--;
+            }
+        }
+        while(stack.peek() != -1) {
+            maxArea = Math.max(maxArea, height(matrix, row, stack.pop()) * (matrix[0].length - stack.peek() - 1));
+        }
+        return maxArea;
+    }
+    
+    // 计算高度这一步可以简化重复计算
+    private int height(char[][] matrix, int row, int col) {
+        int height = 0;
+        while(row >= 0) {
+            if(matrix[row][col] == '1') {
+                height++;
+            } else {
+                break;
+            }
+            row--;
+        }
+        return height;
+    }
+}
+
+class Solution {
+    // 另一种写法
+    public int maximalRectangle(char[][] matrix) {
+        if (matrix.length == 0 || matrix[0].length == 0) {
+            return 0;
+        }
+        int maxArea = 0;
+        int[] height = new int[matrix[0].length];
+        for (int i = 0; i < matrix.length; i++) {
+            maxArea = Math.max(maxArea, helper(matrix, i, height));
+        }
+        return maxArea;
+    }
+
+    private int helper(char[][] matrix, int row, int[] height) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        stack.push(-1);
+        int maxArea = 0;
+        // 改进 height 的计算，其实是二维 DP 数组可以用一维解决，height(row, col) = matrix[row][col] == 1 ? height(row - 1, col) + 1 : 0
+        for (int i = 0; i < matrix[0].length; i++) {
+            height[i] = matrix[row][i] == '1' ? height[i] + 1 : 0;
+        }
+        for (int i = 0; i < matrix[0].length; i++) {
+            if (stack.peek() == -1 || height[i] > height[stack.peek()]) {
+                stack.push(i);
+            } else {
+                maxArea = Math.max(maxArea, height[stack.pop()] * (i - stack.peek() - 1));
+                i--;
+            }
+        }
+        while (stack.peek() != -1) {
+            maxArea = Math.max(maxArea, height[stack.pop()] * (matrix[0].length - stack.peek() - 1));
+        }
+        return maxArea;
+    }
+}
+
+class Solution {
+    // 二维 DP 版本，时间复杂度为 O(n^2)
+    public int maximalRectangle(char[][] matrix) {
+        if (matrix.length == 0 || matrix[0].length == 0) {
+            return 0;
+        }
+        int maxArea = 0;
+        // 三个数组都是 DP 状态数组，有一维数组解决二维的问题，每一行的高、左、右都是跟上一行相关
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int[] height = new int[cols];
+        int[] left = new int[cols];
+        int[] right = new int[cols];
+        Arrays.fill(right, cols);
+        for(int i = 0; i < rows; i++) {
+            int cur_left = 0;
+            int cur_right = cols;
+            for(int j = 0; j < cols; j++) {
+                if (matrix[i][j] == '1') {
+                    height[j]++;
+                } else {
+                    height[j] = 0;
+                }
+            }
+            for(int j = 0; j < cols; j++) {
+                if(matrix[i][j] == '1') {
+                    // left(row, col) = matrix[row][col] == 1 ? max(left(row - 1, col), cur_left)
+                    left[j] = Math.max(left[j], cur_left); // left[j] 默认为 0
+                } else {
+                    left[j] = 0;
+                    cur_left = j + 1;
+                }
+            }
+            for(int j = cols - 1; j >= 0; j--) {
+                if (matrix[i][j] == '1') {
+                    right[j] = Math.min(right[j], cur_right); // right[j] 默认为 cols
+                } else {
+                    right[j] = cols;
+                    cur_right = j;
+                }
+            }
+            for(int j = 0; j < cols; j++) {
+                maxArea = Math.max(maxArea, (right[j] - left[j]) * height[j]);
+            }
+        }
+
+        return maxArea;
+    }
+}
