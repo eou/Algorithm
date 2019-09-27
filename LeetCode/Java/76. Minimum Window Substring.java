6be1350402164648f6ba3d081e8e617160e5b496
eou <whythuss@gmail.com>
2019-09-27 00:12:14 -0500

    🥶

--- LeetCode/Java/76. Minimum Window Substring.java
+++ LeetCode/Java/76. Minimum Window Substring.java
@@ -45,3 +45,62 @@ class Solution {
         return r;
     }
 }
+
+class Solution {
+    public String minWindow(String s, String t) {
+        if (t == null || t.length() == 0 || s == null || s.length() == 0) {
+            return "";
+        }
+
+        String res = "";
+        Map<Character, Integer> map = new HashMap<>();
+        for (Character c : t.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        // move end pointer every loop
+        // only move start pointer when any start chars are unneccessary
+        int start = 0, end = 0;
+        int len = 0;
+        while (end < s.length()) {
+            // move end pointer
+            char c_end = s.charAt(end);
+            if (map.containsKey(c_end)) {
+                map.put(c_end, map.get(c_end) - 1);
+                // only when chars are neccessary, we count it as effective
+                if (map.get(c_end) >= 0) {
+                    len++;
+                }
+            }
+            end++;
+
+            // move start pointer, to narrow down the length
+            while (start < end) {
+                char c_start = s.charAt(start);
+                if (!map.containsKey(c_start)) {
+                    start++;
+                } else {
+                    // have redundant chars
+                    if (map.get(c_start) < 0) {
+                        map.put(c_start, map.get(c_start) + 1);
+                        start++;
+                    } else {
+                        // meet some neccessary chars, not skip
+                        break;
+                    }
+                }
+            }
+
+            // update substring
+            if (len >= t.length()) {
+                if (res == "" || res.length() > end - start) {
+                    res = s.substring(start, end);
+                }
+            }
+            // System.out.println(start);
+            // System.out.println(end);
+        }
+
+        return res;
+    }
+}
