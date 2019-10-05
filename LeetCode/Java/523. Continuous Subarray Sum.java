42246d03f4fd3fe396ad664c4fddadf3695bbcde
eou <whythuss@gmail.com>
2019-10-05 00:53:36 -0500

    😁

--- LeetCode/Java/523. Continuous Subarray Sum.java
+++ LeetCode/Java/523. Continuous Subarray Sum.java
@@ -1,23 +1,24 @@
 // 523. Continuous Subarray Sum
+// [23,2,6,4,7] => the prefix sum is [23,25,31,35,42] => the remainders are [5,1,1,5,0] => 5 - 5 
 class Solution {
     // 用 Prefix sum array 是O(n^2) 复杂度，用 HashMap 是 O(n) 时间复杂度，而空间复杂度是 O(min(n, k))
     public boolean checkSubarraySum(int[] nums, int k) {
         Map<Integer, Integer> map = new HashMap<>();
-        // 当有 sum % k = 0时可以返回true; 设置为 -1 是为了防止第一个元素就是 k 的倍数: 0 - (-1) = 1不会返回true，而从第二个元素开始，sum 如果是 k 的倍数 1 - (-1) > 1就可以返回true
         map.put(0, -1);
         
         int sum = 0;
-        for (int i = 0;i < nums.length; i++) {
+        for (int i = 0; i < nums.length; i++) {
             sum += nums[i];
+            int remainder = sum;
             if (k != 0) {
-                sum %= k; 
+                remainder = sum % k;   // save remainders
             }
-            if(map.containsKey(sum)) {
-                if(i - map.get(sum) > 1) {
+            if (map.containsKey(remainder)) {
+                if(i - map.get(remainder) > 1) {
                     return true;
                 }
             } else {
-                map.put(sum, i);
+                map.put(remainder, i);
             }
         }
         
@@ -41,8 +42,6 @@ class Solution {
         for (int i = 0; i < prefix.length; i++) {
             for (int j = i + 2; j < prefix.length; j++) {
                 if (k == 0 && (prefix[j] - prefix[i] == 0)) {
-                    System.out.println(j);
-                    System.out.println(i);
                     return true;
                 } else if (k != 0 && (prefix[j] - prefix[i]) % k == 0) {
                     return true;
